ac07064a662cb102623f0147161ecbb60b01734b
kMose <bennegron62@gmail.com>
2024-09-17 12:32:39 -0400

    Add tests and functionality for returnsThree and reciprocal functions

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,18 @@
 function returnsThree() {
-  // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
-  // Your code here
+
+  if((typeof n) != 'number'){
+    throw new TypeError("Invalid Input. Requires Number");
+  }
+  if (n >= 1000000 || n <= 0)
+    throw new RangeError("Ranges are between 1 and 1,000,000");
+
+
+
+  return 1/n;
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,64 @@
-// Your code here
+const {expect} = require("chai");
+const {returnsThree, reciprocal} = require("../problems/number-fun.js");
+
+
+describe("returnsThree Function", function () {
+    it("returns the number '3' ", function(){
+
+        let result = returnsThree();
+
+        expect(result).to.equal(3);
+
+    })
+
+})
+
+describe("Reciprocal function", function (){
+    context("Valid Arguments", function (){
+
+
+        it("Takes a number and returns the reciprocal of that number", function (){
+
+            let number1 = 3;
+            let number2 = 2;
+            let number3 = 5;
+
+            let result1 = reciprocal(number1)
+            let result2 = reciprocal(number2)
+            let result3 = reciprocal(number3)
+
+            expect(result1).to.equal(1/3)
+            expect(result2).to.equal(1/2)
+            expect(result3).to.equal(1/5)
+        })
+    })
+
+    context("Invalid Arguments", function (){
+
+
+        it("returns a RangeError when number is out of bounds <= 1 || >= 1000000", function (){
+
+
+            let big = 1000000;
+            let small = 0;
+
+            expect(() => reciprocal(big)).to.throw(RangeError);
+            expect(() => reciprocal(small)).to.throw(RangeError);
+        })
+
+        it("returns an error on general invalid input", function (){
+
+            let input1 = null;
+            let input2 = 'foo';
+            let input3 = ["i like turtles"];
+
+            expect(() => reciprocal(input1)).to.throw(TypeError);
+            expect(() => reciprocal(input2)).to.throw(TypeError);
+            expect(() => reciprocal(input3)).to.throw(TypeError);
+
+        })
+    })
+
+
+
+})
