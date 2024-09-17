f84ef14e1731bc799b34af6405924b2cd35142b9
kMose <bennegron62@gmail.com>
2024-09-17 11:06:57 -0400

    add test for reverseString

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,11 @@
-// Your code here
+const { expect } = require('chai')
+const reverseString = require("../problems/reverse-string.js");
+
+describe("reverseString Function", function (){
+    it("Reverses the word fun to nuf", function (){
+        let word1 = "fun";
+        let result = reverseString(word1);
+
+        expect(result).to.equal("nuf")
+    })
+})
