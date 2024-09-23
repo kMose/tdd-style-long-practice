7bfecb7dd73c32c28c36594a33ea635a69d6beca
kMose <bennegron62@gmail.com>
2024-09-23 09:45:16 -0400

    Implement and test getperimiter function

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,11 @@
-// Your code here
+class Triangle{
+    constructor(side1, side2, side3){
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+}
+
+
+
+module.exports = {Triangle}
--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -3,7 +3,7 @@ const {Triangle} = require("../problems/triangle.js")
 
 
 describe("Constructor", function (){
-    it("Takes length of three sides, side1,side2,side3 and sets them as instance properties" function (){
+    it("Takes length of three sides, side1,side2,side3 and sets them as instance properties", function (){
         let triangle1 = new Triangle(5, 5, 5);
 
         expect(triangle1).to.have.property('side1', 5)
